5a97f1152079fd0dea53ff5e32f6f6f693a496a7
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-07-04 21:02:03 +0300

    [SS-15] реализован интерфейс для проверки прав доступа, удален неиспользуемый Credentials.class

---
+++ src/main/java/com/example/scheduler/application/service/SecurityService.java
@@ -0,0 +1,29 @@
+package com.example.scheduler.application.service;
+
+import com.example.scheduler.domain.model.Credential;
+import com.example.scheduler.domain.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component("security")
+public class SecurityService {
+
+    private final EventRepository repository;
+
+    @Autowired
+    public SecurityService(EventRepository eventRepository) {
+        this.repository = eventRepository;
+    }
+
+    public boolean isOwner(UUID eventId) {
+
+        UUID authUUID = ((Credential) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+
+        return repository.findById(eventId)
+                .map(event -> event.ownerId().equals(authUUID))
+                .orElse(false);
+    }
+}
