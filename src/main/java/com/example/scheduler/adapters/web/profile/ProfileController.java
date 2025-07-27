12c6845ba65a51d17924b50879f87c2b79695b8d
Rossana <rossanatotolo@yandex.ru>
2025-07-27 11:23:56 +0300

    [SS-109] fix global error handler

--- src/main/java/com/example/scheduler/adapters/web/profile/ProfileController.java
+++ src/main/java/com/example/scheduler/adapters/web/profile/ProfileController.java
@@ -4,18 +4,12 @@ import com.example.scheduler.adapters.dto.CreateProfileRequest;
 import com.example.scheduler.adapters.dto.ProfileResponse;
 import com.example.scheduler.adapters.dto.UpdateProfileRequest;
 import com.example.scheduler.application.service.ProfileService;
-import com.example.scheduler.domain.exception.NotEnoughAuthorityException;
-import com.example.scheduler.domain.exception.ProfileAlreadyExistException;
-import com.example.scheduler.domain.exception.ProfileNotFoundException;
-import com.example.scheduler.domain.exception.UserNotAuthorizedException;
 import com.example.scheduler.domain.model.Credential;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,29 +73,5 @@ public class ProfileController {
         log.info("Responded with profile updated for user {}", userId);
         log.debug("Profile updated = {}", response);
         return response;
-    }
-
-    // TODO: Remove after [SS-40] merged
-    @ExceptionHandler
-    public ProblemDetail handleException(UserNotAuthorizedException exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
-    }
-
-    // TODO: Remove after [SS-40] merged
-    @ExceptionHandler
-    public ProblemDetail handleException(NotEnoughAuthorityException exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, exception.getMessage());
-    }
-
-    // TODO: Remove after [SS-40] merged
-    @ExceptionHandler
-    public ProblemDetail handleException(ProfileAlreadyExistException exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, exception.getMessage());
-    }
-
-    // TODO: Remove after [SS-40] merged
-    @ExceptionHandler
-    public ProblemDetail handleException(ProfileNotFoundException exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 }
