c24c25ac80267d772f1ce56951e83f6656fa8cbe
Andrei Kuniutka <akuniutka@gmail.com>
2025-08-02 15:56:58 +0300

    [SS-106] Test: Add end-to-end tests for user registration/login
    - Add end-to-end tests for the following cases:
      - A user registers,
      - A user logs in,
      - A user refreshes tokens.
    - Drop previous version of integration tests for /auth/** endpoints
      (with mocks and for web layer only) in favour of the new end-to-end
      tests.
    - Inject a system clock bean into JwtUtil in order to stabilize tests
      with tokens.
    - Update project settings to prevent JVM warnings while running tests.

---
+++ src/test-integration/java/com/example/scheduler/test/web/auth/UserLoginIT.java
@@ -0,0 +1,95 @@
+package com.example.scheduler.test.web.auth;
+
+import com.example.scheduler.test.config.IntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.json.JsonCompareMode;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@IntegrationTest
+class UserLoginIT {
+
+    private static final String HOST = "http://localhost";
+    private static final String BASE_URL = "/auth/login";
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void givenUserNotRegistered_WhenLogin_ThenRespondWithUnauthorized() {
+        webClient().post()
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                          "username": "charlie",
+                          "password": "12345"
+                        }
+                        """)
+                .exchange()
+                .expectStatus().isUnauthorized()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().json("""
+                        {
+                          "timestamp": "2001-02-03T04:05:06Z",
+                          "status": 401,
+                          "error": "Unauthorized",
+                          "message": "Username or password does not match",
+                          "path": "/auth/login"
+                        }
+                        """, JsonCompareMode.STRICT);
+    }
+
+    @Test
+    void givenRegisteredUserWithWrongPassword_WhenLogin_ThenRespondWithUnauthorized() {
+        webClient().post()
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                          "username": "alice",
+                          "password": "54321"
+                        }
+                        """)
+                .exchange()
+                .expectStatus().isUnauthorized()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().json("""
+                        {
+                          "timestamp": "2001-02-03T04:05:06Z",
+                          "status": 401,
+                          "error": "Unauthorized",
+                          "message": "Username or password does not match",
+                          "path": "/auth/login"
+                        }
+                        """, JsonCompareMode.STRICT);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"alice", "ALICE"})
+    void givenRegisteredUserIgnoreUsernameCase_WhenLogin_ThenRespondWithOkAndAuthResponse(String username) {
+        webClient().post()
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                          "username": "%s",
+                          "password": "12345"
+                        }
+                        """.formatted(username))
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.size()").isEqualTo(2)
+                .jsonPath("$.accessToken").isNotEmpty()
+                .jsonPath("$.refreshToken").isNotEmpty();
+    }
+
+    private WebTestClient webClient() {
+        return WebTestClient.bindToServer().baseUrl(HOST + ":" + port + BASE_URL).build();
+    }
+}
