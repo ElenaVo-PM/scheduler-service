3b432d5111810e4ab1d8d10f3b0cd7fa26fc07a5
Andrei Kuniutka <akuniutka@gmail.com>
2025-08-12 22:15:05 +0300

    [SS-31] Fix: Fix annotation for PublicEventController

--- src/main/java/com/example/scheduler/adapters/web/event/PublicEventController.java
+++ src/main/java/com/example/scheduler/adapters/web/event/PublicEventController.java
@@ -2,14 +2,14 @@ package com.example.scheduler.adapters.web.event;
 
 import com.example.scheduler.adapters.dto.PublicEventResponse;
 import com.example.scheduler.application.service.PublicEventService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/api/public/event")
 public class PublicEventController {
     private final PublicEventService publicEventService;
