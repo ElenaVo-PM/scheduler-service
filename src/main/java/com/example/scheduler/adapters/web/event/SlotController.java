3a230118cef898e14199a27f26f269a1078174ce
Andrei Kuniutka <akuniutka@gmail.com>
2025-08-13 21:03:43 +0300

    [SS-163] Fix: Fix annotation for SlotController

--- src/main/java/com/example/scheduler/adapters/web/event/SlotController.java
+++ src/main/java/com/example/scheduler/adapters/web/event/SlotController.java
@@ -5,15 +5,15 @@ import com.example.scheduler.domain.model.Credential;
 import com.example.scheduler.domain.model.Slot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/api/slots")
 public class SlotController {
     private final SlotService slotService;
